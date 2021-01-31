9b384ea93ee49b9f8a2c903822c685585234e176
Cesar <cesar_silk321@hotmail.com>
2021-01-31 07:34:39 -0300

    Sistema Finalizado

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,21 +4,29 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { AuthComponent } from './auth.component';
-import {NbButtonModule, NbCardModule, NbCheckboxModule, NbLayoutModule, NbSpinnerModule} from "@nebular/theme";
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbInputModule,
+  NbLayoutModule,
+  NbSpinnerModule
+} from "@nebular/theme";
 import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [LoginComponent, AuthComponent],
-    imports: [
-        CommonModule,
-        AuthRoutingModule,
-        NbLayoutModule,
-        NbCardModule,
-        NbButtonModule,
-        NbCheckboxModule,
-        ReactiveFormsModule,
-        NbSpinnerModule
-    ]
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    NbLayoutModule,
+    NbCardModule,
+    NbButtonModule,
+    NbCheckboxModule,
+    ReactiveFormsModule,
+    NbSpinnerModule,
+    NbInputModule
+  ]
 })
 export class AuthModule { }
